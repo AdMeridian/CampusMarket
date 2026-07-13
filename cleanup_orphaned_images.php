5846fecc55e48c02545d75a071ea57b5c4fba169
Carlm832 <carltonnjenga@gmail.com>
2026-07-13 12:41:23 +0300

    Fix database config require path in cleanup script

--- cleanup_orphaned_images.php
+++ cleanup_orphaned_images.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/config/db.php';
 require_once __DIR__ . '/config/supabase.php';
 require_once __DIR__ . '/includes/functions.php';
 
