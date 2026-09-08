751186f7ad13fca393db5e67cfc465bca7ed68aa
Carlm832 <carltonnjenga@gmail.com>
2026-09-08 22:31:54 +0300

    fix(layout): remove excessive mobile top whitespace below search bar on all pages

--- pages/profile.php
+++ pages/profile.php
@@ -93,9 +93,9 @@ include '../includes/header.php';
     overflow: hidden;
 }
 
-@media (max-width: 768px) {
+@media (max-width: 1023px) {
     .profile-hero {
-        padding: calc(70px + 0.65rem) 0 0;
+        padding: 1.25rem 0 0;
     }
 }
 
